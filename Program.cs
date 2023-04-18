d550f7fd3e1e15fb4adeb11208ec871075bb6b8a
DESKTOP-HUDNBUM\sam <sam@super-royal.com>
2023-04-18 16:26:04 +0800

    feat: 新增 echo 功能

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using McMaster.Extensions.CommandLineUtils;
+using System.Globalization;
+
+public class Program
+{
+    static int Main(string[] args)
+    {
+        var app = new CommandLineApplication { Name = "csharp.cli.app" };
+        app.HelpOption("-?|-h|--help");
+        app.OnExecute(() =>
+        {
+            app.ShowHelp();
+            return 0;
+        });
+        _ = app.Command("echo", command =>
+        {
+            command.Description = "輸出用戶輸入的文字。";
+            command.HelpOption("-?|-h|-help");
+            var wordsArgument = command.Argument("[words]", "指定需要輸出的文字。");
+            var repeatOption = command.Option("-r|--repeat-count", "指定輸出重複次數", CommandOptionType.SingleValue);
+            var upperOption = command.Option("--upper", "指定是否全部大寫", CommandOptionType.NoValue);
+            command.OnExecute(() =>
+            {
+                var subject = wordsArgument.HasValue ? wordsArgument.Value : "world";
+                if(upperOption.HasValue())
+                {
+                    subject = subject.ToUpper(CultureInfo.InvariantCulture);
+                }
+                var count = repeatOption.HasValue() ? repeatOption.Value() : "1";
+                int repeat_count = int.Parse(count);
+                for (int i = 0; i < repeat_count; ++i)
+                {
+                    Console.WriteLine(subject);
+                }
+                return 0;
+            });
+        });
+
+        return app.Execute(args);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# csharp.cli
+# cs.cli
 csharp cli
+
+- 建立專案
+```bash=
+dotnet new console -lang c#
+```
+- 在 *.csproj 下新增下面三行
+```xml=
+<PackAsTool>true</PackAsTool>
+<ToolCommandName>csharp.cli</ToolCommandName>
+<PackageOutputPath>./nupkg</PackageOutputPath>
+```
+- 產生 nupkg
+```bash=
+dotnet pack
+```
+- 安裝全域套件
+```bash=
+dotnet tool install --global --add-source .\nupkg csharp.cli
+```
+- 查看全域套件
+```bash=
+dotnet tool list -g
+```
+- 解除安裝全域套件
+```bash=
+dotnet tool uninstall -g csharp.cli
+```
