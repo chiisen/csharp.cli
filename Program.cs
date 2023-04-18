212b3ff7a2443d448635aa23fa825d7ee4c9865f
DESKTOP-HUDNBUM\sam <sam@super-royal.com>
2023-04-18 17:33:46 +0800

    feat: 新增 .NET Polly 重試機制

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using McMaster.Extensions.CommandLineUtils;
+using Polly;
+using System;
 using System.Globalization;
+using System.Net;
 
 public class Program
 {
@@ -37,6 +40,54 @@ public class Program
             });
         });
 
+        _ = app.Command("polly", command =>
+        {
+            command.Description = "重試測試。";
+            command.HelpOption("-?|-h|-help");
+            command.OnExecute(() =>
+            {
+                Policy
+                    // 1. 處理甚麼樣的例外
+                    .Handle<HttpRequestException>()
+
+                    //    或者返回條件(非必要)
+                    .OrResult<HttpResponseMessage>(r => r.StatusCode != HttpStatusCode.OK)
+
+                    // 2. 重試策略，包含重試次數
+                    .Retry(3, (reponse, retryCount, context) =>
+                    {
+                        var result = reponse.Result;
+                        if (result != null)
+                        {
+                            var errorMsg = result.Content
+                                                 .ReadAsStringAsync()
+                                                 .GetAwaiter()
+                                                 .GetResult();
+                            Console.WriteLine($"標準用法，發生錯誤：{errorMsg}，第 {retryCount} 次重試");
+                        }
+                        else
+                        {
+                            var exception = reponse.Exception;
+                            Console.WriteLine($"標準用法，發生錯誤：{exception.Message}，第 {retryCount} 次重試");
+                        }
+
+                        Thread.Sleep(3000);
+                    })
+
+                    // 3. 執行內容
+                    .Execute(FailResponse);
+
+                Console.WriteLine("標準用法，完成");
+            });
+        });
+
         return app.Execute(args);
+    }
+
+    static HttpResponseMessage FailResponse()
+    {
+        HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+        httpResponseMessage.StatusCode = HttpStatusCode.BadGateway;
+        return httpResponseMessage;
     }
 }
