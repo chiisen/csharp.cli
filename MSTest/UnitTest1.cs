11cd0f3b07f645d6292efe071b62d148ed7cb889
sam <chiisen@hotmail.com>
2023-06-26 11:29:32 +0800

    feat: MSTest 新增 Fluent Assertions 語法

--- MSTest/UnitTest1.cs
+++ MSTest/UnitTest1.cs
@@ -1,16 +1,74 @@
+using FluentAssertions;
+
 namespace MSTest
 {
+    public class TestClass
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_Add()
         {
             int num1 = 1;
             int num2 = 2;
-            bool result = num1 == num2;
+            var actual = TestClass.Add(num1, num2);
 
-            Assert.IsFalse(result, "1 should not be prime");
+            actual.Should().Be(num1 + num2, because: $"{num1}+{num2}={num1 + num2}");
         }
-    }
+        [TestMethod]
+        public void TestMethod_Basic()
+        {
+            // object
+            object? obj = null;
+            obj.Should().BeNull("because the obj is null");
+            //obj.Should().NotBeNull();
+
+            // string 
+            var something = "something";
+            //something.Should().BeEmpty();
+            something.Should().NotBeEmpty();
+
+            // datetime 
+            /*
+            var time = new DateTime();
+            time.Should().HaveYear(2023);
+            time.Should().HaveDay(26);
+            time.Should().HaveMonth(6);
+            time.Should().HaveHour(11);
+            time.Should().HaveMinute(25);
+            time.Should().HaveSecond(0);
+            */
+
+            // dictionary 
+            var dic = new Dictionary<int, string>()
+            {
+                {1, "Marcus"},
+                {2, "Flash"},
+                {3, "Neil"}
+            };
+            dic.Should().NotBeEmpty();
+
+            // type
+            something.Should().BeOfType<string>("because a {0} is set", typeof(string));
+            something.Should().BeOfType(typeof(string), "because a {0} is set", typeof(string));
+
+            // equal 
+            string otherObject = "whatever";
+            //something.Should().Be(otherObject, "because they have the same values");
+            something.Should().NotBe(otherObject);
+
+            // exception 
+            /*
+            something.Should().BeOfType<Exception>()
+                .Which.Message.Should().Be("Other Message");
+            */
+        }
+    } 
 }
