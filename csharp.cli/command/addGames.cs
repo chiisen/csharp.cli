3c6d56d6e01f484e4768dfa4427c02356469f3ff
sam <chiisen@hotmail.com>
2023-10-03 18:10:25 +0800

    feat: 轉成範型

--- csharp.cli/command/addGames.cs
+++ csharp.cli/command/addGames.cs
@@ -57,6 +57,79 @@ Redis 格式:
     14,uygm7axgh91qk,葉賢Deluxe
 
     */
+
+    public class AddGamesProcess
+    {
+        public int Process<T>(AddGamesRedisInfo info, List<Dictionary<int, string>> csvList, string sourceJsonText) where T : PWAWebSiteJDB
+        {
+            var sourceJsonList = Common.JsonDeserialize<List<T>>(sourceJsonText);
+            if (sourceJsonList is null)
+            {
+                Console.WriteLine($"無法轉為 List");
+                return 1;
+            }
+
+            // 取的第一筆廠商資料做為新增遊戲的範本
+            var addGameTemplate = sourceJsonList.FirstOrDefault();
+            if (addGameTemplate is null)
+            {
+                Console.WriteLine($"無法取的一筆資料當範本");
+                return 1;
+            }
+
+            // 建立新增遊戲清單
+            var newGameList = new List<T>(sourceJsonList);
+            var count = 0;
+            foreach (var field in csvList)
+            {
+                var sort = field[(int)AddGamesCsvField.Sort].ToString();
+                var gameId = field[(int)AddGamesCsvField.GameId].ToString();
+                var gameName = field[(int)AddGamesCsvField.GameName].ToString();
+
+                var newGameItem = addGameTemplate.Clone();
+                newGameItem.id = gameId;
+                newGameItem.imageName = gameId;
+                newGameItem.name = gameName;
+                newGameItem.sort = int.Parse(sort);
+                newGameItem.localizationCode = $"{info.LocalCode}{gameId}";
+
+                // 判斷有沒有重複的遊戲代碼
+                var exist = newGameList.Where(x => x.id.Equals(gameId)).Select(x => x).FirstOrDefault();
+                if (exist is not null)
+                {
+                    newGameList.Add((T)newGameItem);
+                }
+                else
+                {
+                    Console.WriteLine($"重複的新增遊戲: [{count}] {gameId} {gameName}", Color.Red);
+                }
+
+                Console.WriteLine($"新增遊戲: [{count}] {gameId} {gameName}");
+                count++;
+            }
+
+            // 將新的遊戲清單寫入檔案
+            var newGameJson = JsonConvert.SerializeObject(newGameList, Formatting.Indented);// 格式化後寫入
+            try
+            {
+                var newGameJsonPath = @$"{System.Environment.CurrentDirectory}\{Path.GetFileNameWithoutExtension(info.JsonPath)}.newGames.json";
+
+                Console.WriteLine($"開始寫入 json 遊戲列表 {newGameJsonPath}");
+
+                File.WriteAllText(newGameJsonPath, newGameJson);
+
+                Console.WriteLine($"結束寫入 json 遊戲列表 {newGameJsonPath}");
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e);
+                throw;
+            }
+
+            return 0;
+        }
+    }
+
     public static void addGames()
     {
         _ = App.Command("add-games", command =>
@@ -142,74 +215,15 @@ Redis 格式:
                 }
 
                 // 轉換為對應的廠商格式
+                var addGameProcess = new AddGamesProcess();
                 switch (tpId)
                 {
                     case "JDB":
                     {
-                        var sourceJsonList = Common.JsonDeserialize<List<PWAWebSiteJDB>>(sourceJsonText);
-                        if (sourceJsonList is null)
+                        if (addGameProcess.Process<PWAWebSiteJDB>(info, csvList, sourceJsonText) == 1)
                         {
-                            Console.WriteLine($"無法轉為 List");
                             return 1;
                         }
-
-                        // 取的第一筆廠商資料做為新增遊戲的範本
-                        var addGameTemplate = sourceJsonList.FirstOrDefault();
-                        if (addGameTemplate is null)
-                        {
-                            Console.WriteLine($"無法取的一筆資料當範本");
-                            return 1;
-                        }
-
-                        // 建立新增遊戲清單
-                        var newGameList = new List<PWAWebSiteJDB>(sourceJsonList);
-                        var count = 0;
-                        foreach (var field in csvList)
-                        {
-                            var sort = field[(int)AddGamesCsvField.Sort].ToString();
-                            var gameId = field[(int)AddGamesCsvField.GameId].ToString();
-                            var gameName = field[(int)AddGamesCsvField.GameName].ToString();
-
-                            var newGameItem = addGameTemplate.Clone();
-                            newGameItem.id = gameId;
-                            newGameItem.imageName = gameId;
-                            newGameItem.name = gameName;
-                            newGameItem.sort = int.Parse(sort);
-                            newGameItem.localizationCode = $"{info.LocalCode}{gameId}";
-
-                            // 判斷有沒有重複的遊戲代碼
-                            var exist = newGameList.Where( x => x.id.Equals(gameId)).Select( x => x).FirstOrDefault();
-                            if (exist is not null)
-                            {
-                                newGameList.Add(newGameItem);
-                            }
-                            else
-                            {
-                                Console.WriteLine($"重複的新增遊戲: [{count}] {gameId} {gameName}", Color.Red);
-                            }
-
-                            Console.WriteLine($"新增遊戲: [{count}] {gameId} {gameName}");
-                            count++;
-                        }
-
-                        // 將新的遊戲清單寫入檔案
-                        var newGameJson = JsonConvert.SerializeObject(newGameList, Formatting.Indented);// 格式化後寫入
-                        try
-                        {
-                            var newGameJsonPath = @$"{System.Environment.CurrentDirectory}\{Path.GetFileNameWithoutExtension(info.JsonPath)}.newGames.json";
-
-                            Console.WriteLine($"開始寫入 json 遊戲列表 {newGameJsonPath}");
-
-                            File.WriteAllText(newGameJsonPath, newGameJson);
-
-                            Console.WriteLine($"結束寫入 json 遊戲列表 {newGameJsonPath}");
-                        }
-                        catch (Exception e)
-                        {
-                            System.Console.WriteLine(e);
-                            throw;
-                        }
-
                         break;
                     }
                     default:
