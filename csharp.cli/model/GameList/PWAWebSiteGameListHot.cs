a018097246af415e6878ee0b0c1c5393889040f0
sam <chiisen@hotmail.com>
2024-01-09 17:40:03 +0800

    feat: 新增 hot 熱門遊戲清單

---
+++ csharp.cli/model/GameList/PWAWebSiteGameListHot.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+
+namespace csharp.cli.model.GameList
+{
+    /// <summary>
+    /// 熱門遊戲清單
+    /// </summary>
+    public class PWAWebSiteGameListHotResponse
+    {
+        /// <summary>
+        /// 遊戲id
+        /// </summary>
+        public string? gameId { get; set; }
+        /// <summary>
+        /// 廠商英文代號
+        /// </summary>
+        public string? thirdPartyId { get; set; }
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int sort { get; set; }
+        /// <summary>
+        /// 遊戲類型
+        /// </summary>
+        public int gameType { get; set; }
+        public PWAWebSiteGameListHotResponse() { }
+        public PWAWebSiteGameListHotResponse(PWAWebSiteGameListHotModel x)
+        {
+            if (x == null)
+            {
+                return;
+            }
+            this.gameId = x.gameId;
+            this.thirdPartyId = x.thirdPartyId;
+            this.sort = x.sort;
+            this.gameType = x.gameType;
+        }
+
+        public string ConvertInsertSQL()
+        {
+            return @"INSERT INTO [dbo].[T_Mobile_Popular_Games] (
+        [gameId]
+        ,[thirdPartyId]
+        ,[sort]
+        ,[gameType]
+    )
+VALUES";
+        }
+
+        public string ConvertValuesSQL()
+        {
+            int[] value = { };
+            return @$"(
+        '{this.gameId}',
+        '{this.thirdPartyId}',
+        {this.sort},
+        {this.gameType}
+    )";
+        }
+    }
+}
+
---
+++ csharp.cli/model/GameList/PWAWebSiteGameListHotModel.cs
@@ -0,0 +1,74 @@
+﻿using csharp.cli.model.TableList;
+
+namespace csharp.cli.model.GameList
+{
+    /// <summary>
+    /// 熱門遊戲清單
+    /// </summary>
+    public class PWAWebSiteGameListHotModel : ITableList
+    {
+        /// <summary>
+        /// 遊戲id
+        /// </summary>
+        public string? gameId { get; set; }
+        /// <summary>
+        /// 廠商英文代號
+        /// </summary>
+        public string? thirdPartyId { get; set; }
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int sort { get; set; }
+        /// <summary>
+        /// 遊戲類型
+        /// </summary>
+        public int gameType { get; set; }
+
+        public string ConvertInsertSQL()
+        {
+            return "";
+        }
+
+        public ITableList ConvertItem(int y, object value)
+        {
+            if (value == null)
+            {
+                return this;
+            }
+
+            switch (y)
+            {
+                case 1:
+                    {
+                        this.gameId = value.ToString();
+                    }
+                    break;
+                case 2:
+                    {
+                        this.thirdPartyId = value.ToString();
+                    }
+                    break;                
+                case 3:
+                    {
+                        int.TryParse(value.ToString(), out int parsedValue);
+                        this.sort = parsedValue;
+                    }
+                    break;
+                case 4:
+                    {
+                        int.TryParse(value.ToString(), out int parsedValue);
+                        this.gameType = parsedValue;
+                    }
+                    break;
+            }
+
+            return this;
+        }
+
+        public string ConvertValuesSQL()
+        {
+            return "";
+        }
+    }
+}
+
