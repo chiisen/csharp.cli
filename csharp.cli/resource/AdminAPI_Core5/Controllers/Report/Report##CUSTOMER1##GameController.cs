8f7be70682e3e99711249a33472c346f74a6e2db
sam <chiisen@hotmail.com>
2024-05-20 16:39:41 +0800

    ✨ feat: 新增第三層注單

--- csharp.cli/resource/AdminAPI_Core5/Controllers/Report/Report##CUSTOMER1##GameController.cs
+++ csharp.cli/resource/AdminAPI_Core5/Controllers/Report/Report##CUSTOMER1##GameController.cs
@@ -6,6 +6,11 @@ namespace APPAPI.Controllers
 {
     public partial class ReportController : BaseController
     {
+        /// <summary>
+        /// EGSLOT 第二層注單
+        /// </summary>
+        /// <param name="requestData"></param>
+        /// <returns></returns>
         [Route("History/##CUSTOMER##")]
         [HttpPost]
         public async Task<IActionResult> Get##CUSTOMER1##GameReportHistory([FromBody] W1GetBetRecordRequestModel requestData)
@@ -20,6 +25,34 @@ namespace APPAPI.Controllers
             }
 
             var result = await this._##CUSTOMER2##ApiDataLayer.GetBetRecord(requestData);
+
+            return Json(result);
+        }
+        /// <summary>
+        /// 取得注單第三層報表
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [Route("Detail/##CUSTOMER##")]
+        [HttpPost]
+        public async Task<IActionResult> Get##CUSTOMER1##GameReportDetial([FromBody] W1GetBetDetailRequestModel request)
+        {
+            if (string.IsNullOrEmpty(request.record_id))
+            {
+                throw new ValidateException("Record Id is Null");
+            }
+
+            if (string.IsNullOrEmpty(request.lang))
+            {
+                throw new ValidateException("Lang is Null");
+            }
+
+            if (string.IsNullOrEmpty(request.ReportTime))
+            {
+                throw new ValidateException("ReportTime is Null");
+            }
+
+            var result = await this._##CUSTOMER2##ApiDataLayer.GetGameReportDetial(request);
 
             return Json(result);
         }
